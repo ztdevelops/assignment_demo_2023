c00137f1cbcb6c0d12beac79b0bc23b9e07b7bff
Yue Zheng Ting <87757354+ztdevelops@users.noreply.github.com>
2023-06-04 02:45:23 +0800

    Initialise redis components

--- rpc-server/main.go
+++ rpc-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	rpc "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc/imservice"
@@ -9,7 +10,17 @@ import (
 	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
+var (
+	redisClient = &RedisClient{}
+)
+
 func main() {
+	ctx := context.Background()
+	err := redisClient.Initialise(ctx, "redis:6379", "")
+	if err != nil {
+		log.Fatalf("Failed to initialise Redis client, err: %v", err)
+	}
+
 	r, err := etcd.NewEtcdRegistry([]string{"etcd:2379"}) // r should not be reused.
 	if err != nil {
 		log.Fatal(err)
---
+++ rpc-server/redis.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	// "encoding/json"
+	// "time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	cli *redis.Client
+}
+
+func (c *RedisClient) Initialise(ctx context.Context, address string, password string) error {
+	r := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: password,
+		DB:       0,
+	})
+
+	if err := r.Ping(ctx).Err(); err != nil {
+		return err
+	}
+
+	c.cli = r
+	return nil
+}
+
+func (c *RedisClient) CreateMessage(ctx context.Context, rid string, message *Message) error {
+	msg, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	member := &redis.Z{
+		Member: msg,
+		Score:  float64(message.Timestamp),
+	}
+
+	_, err = c.cli.ZAdd(ctx, rid, *member).Result()
+	return err
+}
+
+func (c *RedisClient) RetrieveMessageByRoomId(ctx context.Context, rid string, start int64, end int64, reverse bool) ([]*Message, error) {
+	var (
+		raw      []string
+		messages []*Message
+		err      error
+	)
+
+	if reverse {
+		raw, err = c.cli.ZRevRange(ctx, rid, start, end).Result()
+	} else {
+		raw, err = c.cli.ZRange(ctx, rid, start, end).Result()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, msg := range raw {
+		tmp := &Message{}
+		err := json.Unmarshal([]byte(msg), tmp)
+		if err != nil {
+			return nil, err
+		}
+
+		messages = append(messages, tmp)
+	}
+
+	return messages, nil
+}
